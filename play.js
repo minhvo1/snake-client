f085b9eab4cfef0250471fc0a788410d2c95b75a
minhvo1 <minhvo199@gmail.com>
2022-05-11 23:22:05 +0000

    implemented input.js

--- play.js
+++ play.js
@@ -1,20 +1,7 @@
 //const net = require("net");
-const connect = require("./client")
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.stdout.write("Thanks for playing!\n");
-    process.exit()
-  }
-}
+console.log("Connecting ...");
 setupInput();
 connect();
